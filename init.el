4798f9593c6339ef0dfad219f8aa69f9ab8fcce2
roboworks <arata.sakamaki@okadanet.org>
2022-02-19 17:07:08 +0900

    working

--- init.el
+++ init.el
@@ -1,38 +1,30 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
 (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
-
 
 (package-initialize)
 
+(package-install 'exec-path-from-shell)
+(package-install 'solarized-theme)
+(package-install 'yaml-mode)
+(package-install 'cmake-mode)
+(package-install 'use-package)
+(package-install 'vertico)
+(package-install 'orderless)
+(package-install 'consult)
+
+;; disable crate backup file
 (setq make-backup-files nil)
 (setq auto-save-default nil)
-
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-11" ))
+;; disbable bell
+(setq ring-bell-function 'ignore)
+;; highlight pair
 (electric-pair-mode 1)
-
-(package-install 'exec-path-from-shell)
-(package-install 'flycheck)
-(package-install 'lsp-mode)
-(package-install 'lsp-ui)
-(package-install 'company-lsp)
-(package-install 'company)
-;;(package-install 'python-mode)
-(package-install 'swiper)
-(package-install 'counsel)
-(package-install 'ivy)
-(package-install 'yasnippet)
-(package-install 'yasnippet-snippets)
-(package-install 'dracula-theme)
-(package-install 'ivy-rich)
-;;(package-install 'lsp-html)
-(package-install 'solarized-theme)
-
+;; highlight line
 (global-hl-line-mode t)
+
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
 (transient-mark-mode t)
@@ -40,73 +32,86 @@
 (which-function-mode 1)
 (setq-default tab-width 4 indent-tabs-mode nil)
 
+;; change font
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-11" ))
 ;;(load-theme 'dracula t)
-(load-theme 'solarized-light t)
+;;(load-theme 'solarized-light t)
 
 (exec-path-from-shell-initialize)
 
+;; Enable vertico
+(use-package vertico
+  :init
+  (vertico-mode)
 
-;;(require 'flycheck)
-;;(global-flycheck-mode)
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
 
-;;(require 'company)
-;;(global-company-mode)
-;;(setq company-idle-delay 0)
-;;(setq company-minimum-prefix-length 2)
-;;(setq company-selection-wrap-around t)
-;;(define-key company-active-map (kbd "M-n") nil)
-;;(define-key company-active-map (kbd "M-p") nil)
-;;(define-key company-active-map (kbd "C-n") 'company-select-next)
-;;(define-key company-active-map (kbd "C-p") 'company-select-previous)
-;;(define-key company-active-map (kbd "C-h") nil)
-;;(define-key company-active-map (kbd "C-i") 'company-complete-selection)
-;;(define-key company-active-map [tab] 'company-complete-selection)
+  ;; Show more candidates
+  (setq vertico-count 20)
 
-;;(defvar company-mode/enable-yas t
-;;  "Enable yasnippet for all backends.")
-;;(defun company-mode/backend-with-yas (backend)
-;;  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-;;	  backend
-;;	(append (if (consp backend) backend (list backend))
-;;			'(:with company-yasnippet))))
-;;(defun set-yas-as-company-backend ()
-;;  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
-;;(add-hook 'company-mode-hook 'set-yas-as-company-backend)
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
 
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
 
+;; Optionally use the `orderless' completion style. See
+;; `+orderless-dispatch' in the Consult wiki for an advanced Orderless style
+;; dispatcher. Additionally enable `partial-completion' for file path
+;; expansion. `partial-completion' is important for wildcard support.
+;; Multiple files can be opened at once with `find-file' if you enter a
+;; wildcard. You may also give the `initials' completion style a try.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
 
-;;(require 'lsp-mode)
-;;(add-hook 'prog-mode-hook #'lsp)
-;;(require 'lsp-ui)
-;;(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
 
-;;(require 'company-lsp)
-;;(push 'company-lsp company-backends)
+(use-package consult
+  :init
+  (define-key global-map (kbd "C-x b") 'consult-buffer)
+  (define-key global-map (kbd "C-s") 'consult-line)
+  (define-key global-map (kbd "C-r") 'consult-line)
+  )
 
-(require 'ivy)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq ivy-extra-directories nil)
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-plus)))
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; Alternatively try `consult-completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(defvar counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-(global-set-key "\C-s" 'swiper)
-(defvar swiper-include-line-number-in-search t)
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
 
-
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (yasnippet ivy swiper company lsp-mode yasnippet-snippets lsp-ui lsp-html ivy-rich flycheck exec-path-from-shell dracula-theme counsel company-lsp auto-highlight-symbol))))
+   '(consult orderless use-package yasnippet-snippets yaml-mode vertico solarized-theme markdown-preview-mode lsp-ui ivy-rich flycheck exec-path-from-shell dracula-theme counsel company cmake-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
